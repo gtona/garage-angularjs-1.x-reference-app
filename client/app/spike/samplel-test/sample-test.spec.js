f9e6e4b82ce08298c20e158def9db2372f84f308
WIL PANNELL <wpannell@gmail.com>
2016-03-18 12:43:15 +0530

    add stub for sample test

---
+++ client/app/spike/samplel-test/sample-test.spec.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+let expect = chai.expect;
+
+describe('the test infrastructure works', () => {
+  it('should add 2 + 2', () => {
+    expect(2 + 2).to.equall(4);
+  });
+});
